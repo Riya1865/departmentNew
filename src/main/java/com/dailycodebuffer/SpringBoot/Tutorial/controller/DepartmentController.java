89a91ae00a405de2bc09ee922d36725067729e6a
Riya Singla <riyasingla@Riyas-MacBook-Pro.local>
2023-04-27 00:20:49 +0530

    first commit

--- src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/controller/DepartmentController.java
@@ -1,7 +1,19 @@
 package com.dailycodebuffer.SpringBoot.Tutorial.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dailycodebuffer.SpringBoot.Tutorial.entity.Department;
+import com.dailycodebuffer.SpringBoot.Tutorial.service.DepartmentService;
+import com.dailycodebuffer.SpringBoot.Tutorial.service.DepartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment (@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
 }
